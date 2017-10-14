ca7aed3a256cf796a93fac5c14f55bf0e47dde3b
Steven Lian <stevenlian1994@gmail.com>
2017-10-14 06:24:02 +0000

    second commit, added uniformity in header, navbar, etc through partials

--- index.js
+++ index.js
@@ -3,15 +3,24 @@ var express = require("express"),
 
 app.set("view engine", "ejs");
 
+app.use(express.static(__dirname + '/views'));
+
 
 app.get("/", function(req, res){
     res.render("main");
 });
 
+app.get("/projects", function(req, res){
+    res.render("projects");
+})
 
+app.get("/about", function(req, res){
+    res.render("about");
+})
 
-
-
+app.get("/contact", function(req, res){
+    res.render("contact");
+})
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Homepage has started!");
